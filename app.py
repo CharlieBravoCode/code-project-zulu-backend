401dd8863c0c401d07d65380accbcbfbc5f6351e
CharlieBravoCode <89343590+CharlieBravoCode@users.noreply.github.com>
2022-04-27 19:36:42 +0200

    Refactor in files models and routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify,request, abort
+from flask import Flask, jsonify,request
 from flask_sqlalchemy import SQLAlchemy
 import os
 import psycopg2
@@ -61,31 +61,6 @@ login_manager.init_app(app)
 
 #______________________ Database Tables ______________________ #
 
-class Events(db.Model):
-    __tablename__ = 'events'
-
-    id = db.Column(db.Integer, primary_key = True)
-    identifier = db.Column(db.String)
-    title = db.Column(db.String)
-    location = db.Column(db.Integer())
-    latitud = db.Column(db.Float)
-    longitud = db.Column(db.Float)
-
-    def __repr__(self):
-        return "<Event %r>" % self.title 
-
-
-class User(db.Model, UserMixin):
-    __tablename__ = "user"
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), unique=True, nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(255))
-    #active = db.Column(db.Boolean)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
 
 
 
@@ -118,107 +93,6 @@ def login():
 
 
 #____________Routes - Content_______________ #
-
-@cross_origin()
-@app.route('/events', methods = ['POST'])
-def create_event():
-    event_data = request.json
-
-    identifier = event_data['identifier']
-    title  = event_data['title']
-    location = event_data['location']
-    latitud = event_data['latitud']
-    longitud = event_data['longitud']
-  
-    event = Events(identifier = identifier, title = title, location = location, latitud = latitud, longitud = longitud)
-    db.session.add(event)
-    db.session.commit()
-    
-
-    return jsonify({"success": True,"response":"Event added"})
-
-
-@cross_origin()  
-@app.route("/")
-def home():
-    return "Hello World!"
-
-@cross_origin()    
-@app.route('/events', methods = ['GET'])
-def getevents():
-     all_events = []
-     events = Events.query.all()
-     for event in events:
-          results = {
-                    "id":event.id,
-                    "identifier":event.identifier,
-                    "title":event.title,
-                    "location":event.location,
-                    "latitud":event.latitud,
-                    "longitud":event.longitud
-          }
-          all_events.append(results)
-
-     return jsonify(all_events)
-
-
-@cross_origin() 
-@app.route('/events/geojson', methods = ['GET'])
-def geteventsgeojson():
-        points = []
-        events = Events.query.all()
-        for event in events:
-            points.append({
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [event.longitud, event.latitud]
-                },
-                "properties": {
-                    "title": event.title,
-                    "identifier": event.identifier,
-                    "location": event.location,
-                    "id": event.id
-                }
-            })
-        return jsonify({"type": "FeatureCollection", "features": points})
-
-
-
-@cross_origin()  
-@app.route("/events/<int:event_id>", methods = ["PUT"])
-def update_event(event_id):
-    event = Events.query.get(event_id)
-    identifier = request.json['identifier']
-    title = request.json['title']
-    location = request.json['location']
-    latitud = request.json['latitud']
-    longitud = request.json['longitud']
-
-    if event is None:
-        abort(404)
-    else:
-        event.identifier = identifier
-        event.title = title
-        event.location = location
-        event.latitud = latitud
-        event.longitud = longitud
-        db.session.add(event)
-        db.session.commit()
-        return jsonify({"success": True, "response": "Event Details updated"})
-
-
-@cross_origin()  
-@app.route("/events/<int:event_id>", methods = ["DELETE"])
-def delete_event(event_id):
-    event = Events.query.get(event_id)
-
-    if event is None:
-        abort(404)
-    else:
-        db.session.delete(event)
-        db.session.commit()
-        return jsonify({"success": True, "response": "Event Deleted"})
 
 
 
---
+++ models.py
@@ -0,0 +1,28 @@
+from app import db
+from flask_login import (UserMixin, login_user, LoginManager, current_user, logout_user, login_required)
+
+class Events(db.Model):
+    __tablename__ = 'events'
+
+    id = db.Column(db.Integer, primary_key = True)
+    identifier = db.Column(db.String)
+    title = db.Column(db.String)
+    location = db.Column(db.Integer())
+    latitud = db.Column(db.Float)
+    longitud = db.Column(db.Float)
+
+    def __repr__(self):
+        return "<Event %r>" % self.title 
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = "user"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(255))
+    #active = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
