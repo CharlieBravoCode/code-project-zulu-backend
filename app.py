9acd6af112feca38a1ee91a6e4395a6b796b1372
CharlieBravoCode <89343590+CharlieBravoCode@users.noreply.github.com>
2022-04-27 20:06:28 +0200

    reverse to one monster app-file

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ from flask_cors import CORS, cross_origin
 #from flask_migrate import Migrate
 
 
-from flask_login import (UserMixin, login_user, LoginManager, current_user, logout_user, login_required)
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user, login_required
 
 
 
-#______________________ Create App ______________________ #
+#______________________  App ______________________ #
 app = Flask(__name__)
 CORS(app)
 
@@ -62,12 +62,164 @@ login_manager.init_app(app)
 
 #______________________ Database Tables ______________________ #
 
+class Events(db.Model):
+    __tablename__ = 'events'
+
+    id = db.Column(db.Integer, primary_key = True)
+    identifier = db.Column(db.String)
+    title = db.Column(db.String)
+    location = db.Column(db.Integer())
+    latitud = db.Column(db.Float)
+    longitud = db.Column(db.Float)
+
+    def __repr__(self):
+        return "<Event %r>" % self.title 
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = "user"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(255))
+    #active = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 
 #______________________ Routes ______________________ #
 
+#____________Routes - User Login_______________ #
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@cross_origin()
+@app.route('/login', methods=['POST'])
+def login():
+    user_data = request.json
+
+    username = user_data['username']
+    password = user_data['password']
+
+    user = User.query.filter_by(username=username).first()
+
+    if user.username == username and user.password == password:
+        login_user(user)
+        return jsonify({'username': user.username})
+
+
+
+
+
+#____________Routes - Content_______________ #
+
+
+@cross_origin()
+@app.route('/events', methods = ['POST'])
+def create_event():
+    event_data = request.json
+
+    identifier = event_data['identifier']
+    title  = event_data['title']
+    location = event_data['location']
+    latitud = event_data['latitud']
+    longitud = event_data['longitud']
+  
+    event = Events(identifier = identifier, title = title, location = location, latitud = latitud, longitud = longitud)
+    db.session.add(event)
+    db.session.commit()
+    
+
+    return jsonify({"success": True,"response":"Event added"})
+
+
+@cross_origin()  
+@app.route("/")
+def home():
+    return "Hello World!"
+
+@cross_origin()    
+@app.route('/events', methods = ['GET'])
+def getevents():
+     all_events = []
+     events = Events.query.all()
+     for event in events:
+          results = {
+                    "id":event.id,
+                    "identifier":event.identifier,
+                    "title":event.title,
+                    "location":event.location,
+                    "latitud":event.latitud,
+                    "longitud":event.longitud
+          }
+          all_events.append(results)
+
+     return jsonify(all_events)
+
+
+@cross_origin() 
+@app.route('/events/geojson', methods = ['GET'])
+def geteventsgeojson():
+        points = []
+        events = Events.query.all()
+        for event in events:
+            points.append({
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [event.longitud, event.latitud]
+                },
+                "properties": {
+                    "title": event.title,
+                    "identifier": event.identifier,
+                    "location": event.location,
+                    "id": event.id
+                }
+            })
+        return jsonify({"type": "FeatureCollection", "features": points})
+
+
+
+@cross_origin()  
+@app.route("/events/<int:event_id>", methods = ["PUT"])
+def update_event(event_id):
+    event = Events.query.get(event_id)
+    identifier = request.json['identifier']
+    title = request.json['title']
+    location = request.json['location']
+    latitud = request.json['latitud']
+    longitud = request.json['longitud']
+
+    if event is None:
+        abort(404)
+    else:
+        event.identifier = identifier
+        event.title = title
+        event.location = location
+        event.latitud = latitud
+        event.longitud = longitud
+        db.session.add(event)
+        db.session.commit()
+        return jsonify({"success": True, "response": "Event Details updated"})
+
+
+@cross_origin()  
+@app.route("/events/<int:event_id>", methods = ["DELETE"])
+def delete_event(event_id):
+    event = Events.query.get(event_id)
+
+    if event is None:
+        abort(404)
+    else:
+        db.session.delete(event)
+        db.session.commit()
+        return jsonify({"success": True, "response": "Event Deleted"})
 
 
 
