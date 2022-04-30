c7d70bb876db30dd1ce288c59e939a50c6b104ce
CharlieBravoCode <89343590+CharlieBravoCode@users.noreply.github.com>
2022-04-30 21:50:14 +0200

    clean up before hand-in_2

--- auth.py
+++ auth.py
@@ -9,7 +9,7 @@ from urllib.request import urlopen
 
 AUTH0_DOMAIN = 'dev-l5guaxfx.eu.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'https://code-project-zulu-backend.herokuapp.com/', 'http://localhost:5000'
+API_AUDIENCE = 'https://code-project-zulu-backend.herokuapp.com/'
 
 
 #______________________  Error Class ______________________ #
