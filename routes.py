f3ee8b9ed3f5128d3390294caf40c06926f861ef
CharlieBravoCode <89343590+CharlieBravoCode@users.noreply.github.com>
2022-11-04 14:48:14 +0100

    moved routes into routes.py

---
+++ routes.py
@@ -0,0 +1,107 @@
+from app import app
+from flask_cors import CORS, cross_origin
+from flask import Flask, jsonify, request
+
+
+@cross_origin()
+@app.route('/events', methods = ['POST'])
+def create_event():
+    event_data = request.json
+
+    identifier = event_data['identifier']
+    title  = event_data['title']
+    location = event_data['location']
+    latitud = event_data['latitud']
+    longitud = event_data['longitud']
+  
+    event = Events(identifier = identifier, title = title, location = location, latitud = latitud, longitud = longitud)
+    db.session.add(event)
+    db.session.commit()    
+
+    return jsonify({"success": True,"response":"Event added"})
+
+
+@cross_origin()  
+@app.route("/")
+def home():
+    return "Hello World!"
+
+
+@cross_origin()    
+@app.route('/events', methods = ['GET'])
+def getevents():
+     all_events = []
+     events = Events.query.all()
+     for event in events:
+          results = {
+                    "id":event.id,
+                    "identifier":event.identifier,
+                    "title":event.title,
+                    "location":event.location,
+                    "latitud":event.latitud,
+                    "longitud":event.longitud
+          }
+          all_events.append(results)
+
+     return jsonify(all_events)
+
+
+@cross_origin() 
+@app.route('/events/geojson', methods = ['GET'])
+def geteventsgeojson():
+        points = []
+        events = Events.query.all()
+        for event in events:
+            points.append({
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [event.longitud, event.latitud]
+                },
+                "properties": {
+                    "title": event.title,
+                    "identifier": event.identifier,
+                    "location": event.location,
+                    "id": event.id
+                }
+            })
+        return jsonify({"type": "FeatureCollection", "features": points})
+
+
+
+@cross_origin()  
+@app.route("/events/<int:event_id>", methods = ["PUT"])
+def update_event(event_id):
+    event = Events.query.get(event_id)
+    identifier = request.json['identifier']
+    title = request.json['title']
+    location = request.json['location']
+    latitud = request.json['latitud']
+    longitud = request.json['longitud']
+
+    if event is None:
+        abort(404)
+    else:
+        event.identifier = identifier
+        event.title = title
+        event.location = location
+        event.latitud = latitud
+        event.longitud = longitud
+        db.session.add(event)
+        db.session.commit()
+        return jsonify({"success": True, "response": "Event Details updated"})
+
+
+
+@cross_origin()  
+@app.route("/events/<int:event_id>", methods = ["DELETE"])
+def delete_event(event_id):
+    event = Events.query.get(event_id)
+
+    if event is None:
+        abort(404)
+    else:
+        db.session.delete(event)
+        db.session.commit()
+        return jsonify({"success": True, "response": "Event Deleted"})
+
