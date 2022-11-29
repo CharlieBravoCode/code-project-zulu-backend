7408e8b3f592f15183ffcbbf1eefa646fe7e4fcf
CharlieBravoCode <89343590+CharlieBravoCode@users.noreply.github.com>
2022-11-29 16:52:50 +0100

    clean up: app.py

--- app.py
+++ app.py
@@ -1,32 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 from cors import setup_cors
-import os
 
 
 app = Flask(__name__)
 CORS(app)
 setup_cors(app)
-
-#______________________ Database Connection Config ______________________ #
-
 db = SQLAlchemy(app)
-basedir = os.path.abspath(os.path.dirname(__file__))
 
-#### Local Dev Postgres Docker ####
-
-app.config['SECRET_KEY'] = 'mysecretkey'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mysecretpassword@localhost:5432'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
-
-
-
-
-#______________________ Other ______________________ #
 
 @app.errorhandler(400)
 def bad_request(error):
@@ -36,6 +18,5 @@ def bad_request(error):
 if __name__ == '__app__':
   app.run(debug=True)
 
-#______________________ Routes ______________________ #
 
 from routes import *
