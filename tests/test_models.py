b6c02e71feb47909d7fb892d9400cb4dfd8d42d8
CharlieBravoCode <89343590+CharlieBravoCode@users.noreply.github.com>
2022-11-29 15:25:15 +0100

    added tests for db.models

---
+++ tests/test_models.py
@@ -0,0 +1,16 @@
+import unittest
+from app import Events
+
+class TestEvents(unittest.TestCase):
+    def setUp(self): 
+        self.event = Events(id=1, identifier="id1", title="My Event", location="NYC", latitud=40.7128, longitud=74.0060)
+
+    def test_title(self):
+        self.assertEqual(self.event.title, "My Event")
+
+    def test_location(self):
+        self.assertEqual(self.event.location, "NYC")
+
+    def test_latitud_and_longitud(self):
+        self.assertEqual(self.event.latitud, 40.7128)
+        self.assertEqual(self.event.longitud, 74.0060)
