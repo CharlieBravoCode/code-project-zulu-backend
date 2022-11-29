e5385954864c60976ebfedb9c8114c19c82c3988
CharlieBravoCode <89343590+CharlieBravoCode@users.noreply.github.com>
2022-11-29 16:50:06 +0100

    clean up: Database ref heroku moved to routes

--- app.py
+++ app.py
@@ -22,22 +22,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mysecretpassword@
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-#### Database in Heroku  ####
-
-# os.environ['DATABASE_URL'] = 'postgresql://iblhdktfqmapza:6a8b46b627797e8879823eccde4731f392eaf852ca37a742b1be3e8fe1c1c531@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/dfeftmig21ojqf'
-
-# DATABASE_URL = os.environ['DATABASE_URL']
-# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-# SQLAlchemy_DATABASE_URI = os.environ['DATABASE_URL']
-# app.config['SQLALCHEMY_DATABASE_URI'] = SQLAlchemy_DATABASE_URI
-
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://iblhdktfqmapza:6a8b46b627797e8879823eccde4731f392eaf852ca37a742b1be3e8fe1c1c531@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/dfeftmig21ojqf'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-#### Database Local ####
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:seglniosi3ng9834ogno3ngkldowuez!$rmfmrRJjmelsmfdjUfnerurnfsegom490zj498t23nto(ugneukgbekgdj@localhost/zulu_db_postgres'
 
 
 
