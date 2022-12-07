ea4631a756f623230b7bc452b906386eb3063fa6
CharlieBravoCode <89343590+CharlieBravoCode@users.noreply.github.com>
2022-12-07 14:06:56 +0100

    clean up: db config to heroku

--- routes.py
+++ routes.py
@@ -10,29 +10,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 #### Local Dev Postgres Docker ####
-
-app.config['SECRET_KEY'] = 'mysecretkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mysecretpassword@localhost:5432'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
 #### Database in Heroku  ####
-
-    # os.environ['DATABASE_URL'] = 'postgresql://iblhdktfqmapza:6a8b46b627797e8879823eccde4731f392eaf852ca37a742b1be3e8fe1c1c531@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/dfeftmig21ojqf'
-
-    # DATABASE_URL = os.environ['DATABASE_URL']
-    # conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-    # SQLAlchemy_DATABASE_URI = os.environ['DATABASE_URL']
-    # app.config['SQLALCHEMY_DATABASE_URI'] = SQLAlchemy_DATABASE_URI
-
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://iblhdktfqmapza:6a8b46b627797e8879823eccde4731f392eaf852ca37a742b1be3e8fe1c1c531@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/dfeftmig21ojqf'
-    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    #### Database Local ####
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:seglniosi3ng9834ogno3ngkldowuez!$rmfmrRJjmelsmfdjUfnerurnfsegom490zj498t23nto(ugneukgbekgdj@localhost/zulu_db_postgres'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://iblhdktfqmapza:6a8b46b627797e8879823eccde4731f392eaf852ca37a742b1be3e8fe1c1c531@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/dfeftmig21ojqf'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
 
 
 
