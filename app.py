df481395a698aab7e8b0fa2b28b33c290fee4101
CharlieBravoCode <89343590+CharlieBravoCode@users.noreply.github.com>
2022-12-08 18:01:32 +0100

    clean up: moved all cors code in cors.py

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
 from cors import setup_cors
 
 
 app = Flask(__name__)
-CORS(app)
-setup_cors(app)
 db = SQLAlchemy(app)
-
-
-@app.errorhandler(400)
-def bad_request(error):
-    return make_response(jsonify({"error": "Bad request"}), 400)
+setup_cors(app)
 
 
 if __name__ == '__app__':
--- cors.py
+++ cors.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
 
 
+
+
 def setup_cors(app):
   cors= CORS(app, resources={
       r"/*": {
