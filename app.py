29f375b701890f6519b09b6df280fa7f8be2d69b
CharlieBravoCode <89343590+CharlieBravoCode@users.noreply.github.com>
2022-12-08 18:13:11 +0100

    clean up: app.py move app.run to the end

--- app.py
+++ app.py
@@ -2,14 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from cors import setup_cors
 
-
 app = Flask(__name__)
 db = SQLAlchemy(app)
 setup_cors(app)
 
+from routes import *
+
 
 if __name__ == '__app__':
   app.run(debug=True)
-
-
-from routes import *
