16854a81e192fb8bb5009be425c140fde1489abf
CharlieBravoCode <89343590+CharlieBravoCode@users.noreply.github.com>
2022-12-08 17:27:26 +0100

    clean up: turned db-config into a function

--- routes.py
+++ routes.py
@@ -5,21 +5,20 @@ from app import app, db
 import os
 
 
-db = SQLAlchemy(app)
-basedir = os.path.abspath(os.path.dirname(__file__))
+def configure_db():
+    basedir = os.path.abspath(os.path.dirname(__file__))
 
+    #### Local Dev Postgres Docker ####
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mysecretpassword@localhost:5432'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db = SQLAlchemy(app)
 
-#### Local Dev Postgres Docker ####
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mysecretpassword@localhost:5432'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+    #### Database in Heroku  ####
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://iblhdktfqmapza:6a8b46b627797e8879823eccde4731f392eaf852ca37a742b1be3e8fe1c1c531@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/dfeftmig21ojqf'
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # db = SQLAlchemy(app)
 
-
-#### Database in Heroku  ####
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://iblhdktfqmapza:6a8b46b627797e8879823eccde4731f392eaf852ca37a742b1be3e8fe1c1c531@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/dfeftmig21ojqf'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
-
+configure_db()
 
 
 class Events(db.Model):
